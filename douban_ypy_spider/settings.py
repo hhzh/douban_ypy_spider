3795f34f6c21f74732b888ddb52dd0c9c5ea6cdf
hhzh <1138253815@qq.com>
2017-11-06 15:51:57 +0800

    add

--- douban_ypy_spider/settings.py
+++ douban_ypy_spider/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'douban_ypy_spider.spiders'
 #USER_AGENT = 'douban_ypy_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
